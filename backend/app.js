26c201dc6bc7d675ef57b2ada143116b36e56c34
Sanjay <sanjaybishtamazon@gmail.com>
2023-03-20 18:33:36 +0530

    Updated COde

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,8 @@ import cors from "cors"
 const app = express()
 
 app.use(cors())
-app.use('/', router);
 app.use(express.json())
+app.use('/', router);
 app.listen(8800, ()=> {
     console.log("Connected to Backend")
 })
